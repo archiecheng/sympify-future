b0c2a8feaee7762df8f89011e185aefc92fa4446
archiecheng <archiechengice@outlook.com>
2024-10-04 14:29:38 -0700

    项目搭建

--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
+/*
+ * @Author: archiecheng archiechengice@outlook.com
+ * @Date: 2024-10-04 14:14:58
+ * @LastEditors: archiecheng archiechengice@outlook.com
+ * @LastEditTime: 2024-10-04 14:25:48
+ * @FilePath: \sympifyfuture\src\main.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入elemetn-ui组件库
+import ElementUI from 'element-ui';
+// 引入element-ui全部css
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
+// 使用element
+Vue.use(ElementUI)
 
 new Vue({
   router,
